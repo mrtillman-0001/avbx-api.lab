0cd82dd4e8cd1cb7ca5009e2487c44485f86bbab
Stephen Tillman <mrtillman.0001@gmail.com>
2021-04-03 16:05:42 -0400

    implement image upload/delete methods

--- avbx-gravatar.client.js
+++ avbx-gravatar.client.js
@@ -23,6 +23,65 @@ class AvBxGravatarClient extends AvBxApiClient {
       headers: this.headers
     }).then(res => res.json());
   }
+  setPrimaryImage(imageName){
+    const url = this.baseUri + 'gravatar/' + imageName;
+    return fetch(url, {
+      method: 'PUT',
+      headers: this.headers
+    }).then(res => res.json());
+  }
+  removePrimaryImage(){
+    const url = this.baseUri + 'gravatar';
+    return fetch(url, {
+      method: 'DELETE',
+      headers: this.headers
+    }).then(res => res.json());
+  }
+  saveImageUrl(imageUrl){
+    const url = this.baseUri + 'gravatar/images';
+    const body = new FormData();
+    body.append('imageUrl', imageUrl);
+    return fetch(url, {
+      method: 'POST',
+      headers: this.headers,
+      body
+    }).then(res => res.json());
+  }
+  saveImageData(imageData){
+    const url = this.baseUri + 'gravatar/images';
+    const body = new FormData();
+    body.append('imageData', imageData);
+    return fetch(url, {
+      method: 'POST',
+      headers: this.headers,
+      body
+    }).then(res => res.json());
+  }
+  saveImageFile(imageFile){
+    const url = this.baseUri + 'gravatar/images';
+    const body = new FormData();
+    body.append('image', imageFile);
+    return fetch(url, {
+      method: 'POST',
+      headers: this.headers,
+      body
+    }).then(res => res.json());
+  }
+  deleteImage(imageName){
+    const url = this.baseUri + 'gravatar/images/' + imageName;
+    return fetch(url, {
+      method: 'DELETE',
+      headers: this.headers
+    }).then(res => res.json());
+  }
+  deleteImages(...imageNames){
+    const url = this.baseUri + 'gravatar/images';
+    return fetch(url, {
+      method: 'DELETE',
+      headers: Object.assign({}, this.headers, { 'Content-Type': 'application/json' }),
+      body: JSON.stringify({ imageNames })
+    }).then(res => res.json());
+  }
   test(){
     const url = this.baseUri + 'gravatar/test';
     return fetch(url, {
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 window.onload = function(){
   var client = new AvBxGravatarClient(token);
+  var fileInput = document.getElementById('fileInput');
   
-  client
-  .getImages()
-  .then(console.log)
-  .catch(console.log);
+  client.deleteImage('3e6cc942c2b18108ef8a6dc5dc22999f');
+
+  fileInput.addEventListener('change', uploadFile);
+
+  function uploadFile(){
+    const file = fileInput.files[0];
+    client.saveImageFile(file)
+    .then(console.log)
+    .catch(console.log);
+  }
+  
+  // client
+  // .test()
+  // //.deleteImages('c848d8bd65b960b315795f5d83887902', 'b441bd4fae0683fa92626a62db9798c5')
+  // //.saveImageData('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=')
+  // //.saveImageUrl('http://placehold.it/512')
+  // .then(console.log)
+  // .catch(console.log);
 
 }
